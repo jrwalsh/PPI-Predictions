13c1f673344b0a3c1b06e071741c7cfcecfe7978
Jesse Walsh <jrwalsh@iastate.edu>
2016-09-14 16:37:57 -0500

    Added comments, test method, and basic statistics method.

--- PPI-Predictions/src/ppi/Main.java
+++ PPI-Predictions/src/ppi/Main.java
@@ -17,11 +17,19 @@ import ppi.preprocessing.NodeFileReader;
 
 
 public class Main {
+	private final String predictedDomains = "/Data/Arabidopsis/pfam_predictions.tab";
+	private final String iPfamDDIs = "/Data/iPfam/iPfam_domain_interactions.tab";
+	private final String highConfidenceGGIs = "/Data/Arabidopsis/high_confidence_interactions.tab";
 	
 	public static void main(String args[]) {
 		Long start = System.currentTimeMillis();
 		
-		crossValidate();
+		test();
+//		basicStatistics();
+//		generateNetworkFromiPfam();
+//		generateNetworkFromAssumedDDIs();
+//		generateConfusionMatrix();
+//		crossValidate();
 		
 		System.out.println("done!");
 		
@@ -31,9 +39,61 @@ public class Main {
 	}
 	
 	/**
+	 * Test run ideas
+	 */
+	private static void test() {
+		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
+		ArrayList<DomainDomainInteraction> DDIs = EdgeFileReader.readDDIList();
+		ArrayList<GeneGeneInteraction> GGIs = EdgeFileReader.readGGIList();
+		
+		HashSet<DomainDomainInteraction> assumed_DDI_set = NetworkFactory.convertGGItoDDI(predictedDomains, GGIs);
+		HashSet<DomainDomainInteraction> DDI_intersection = new HashSet<DomainDomainInteraction>();
+		DDI_intersection.addAll(DDIs);
+		DDI_intersection.retainAll(assumed_DDI_set);
+		ArrayList<DomainDomainInteraction> DDI_set = new ArrayList<DomainDomainInteraction>();
+		DDI_set.addAll(DDI_intersection);
+		
+		UndirectedGraph<String, DefaultEdge> predictedGraph = NetworkFactory.generateNetwork(predictedDomains, DDI_set);
+		ArrayList<GeneGeneInteraction> predictedGGIs = new ArrayList<GeneGeneInteraction>();
+		ArrayList<GeneGeneInteraction> tpGGIs = new ArrayList<GeneGeneInteraction>();
+		predictedGGIs.addAll(NetworkFactory.convertEdgesToGGIs(predictedGraph));
+		tpGGIs.addAll(predictedGGIs);
+		tpGGIs.retainAll(GGIs); // Intersection gives all TP results
+		
+		System.out.println("HCSet\tPredictions\tTP");
+		System.out.println(GGIs.size() + "\t" + predictedGGIs.size() + "\t" + tpGGIs.size());
+		
+		System.out.println("Size of DDI set");
+		System.out.println(DDI_set.size());
+	}
+	
+	private static void basicStatistics() {
+		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
+		ArrayList<DomainDomainInteraction> DDIs = EdgeFileReader.readDDIList();
+		ArrayList<GeneGeneInteraction> GGIs = EdgeFileReader.readGGIList();
+		HashSet<DomainDomainInteraction> assumed_DDI_set = NetworkFactory.convertGGItoDDI(predictedDomains, GGIs);
+		
+		// Stats for the predictedDomains
+		for (PredictedDomain predictedDomain : predictedDomains) {
+			
+		}
+		System.out.println("Predicted Domains (pfam_scan)");
+		System.out.println("Total predictions " + predictedDomains.size());
+		
+		// Which DDI's in the iPfam set are actually predicting known GGIs from the HC set?  How many of them are there?
+		HashSet<DomainDomainInteraction> DDI_intersection = new HashSet<DomainDomainInteraction>();
+		DDI_intersection.addAll(DDIs);
+		DDI_intersection.retainAll(assumed_DDI_set);
+		System.out.println("The overlap between the iPfam DDIs and the assumed DDIs based on the high confidence set is: " + DDI_intersection.size());
+		
+		
+		
+	}
+	
+	/**
 	 * Try generating a network based on iPfam DDIs
 	 */
-	private static void testNetworkPrediction() {
+	private static void generateNetworkFromiPfam() {
 		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
 		ArrayList<DomainDomainInteraction> DDIs = EdgeFileReader.readDDIList();
 		UndirectedGraph<String, DefaultEdge> graph = NetworkFactory.generateNetwork(predictedDomains, DDIs);
@@ -43,13 +103,17 @@ public class Main {
 	/**
 	 * Try generating a network based on all possible DDIs within the HC set
 	 */
-	private static void testAssumedDDIs() {
+	private static void generateNetworkFromAssumedDDIs() {
 		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
 		ArrayList<GeneGeneInteraction> GGIs = EdgeFileReader.readGGIList();
 		UndirectedGraph<String, DefaultEdge> graph = NetworkFactory.generateNetworkGGI(predictedDomains, GGIs);
 	}
 	
+	/**
+	 * Generate a network based on iPfam DDIs and then compare to the high confidence GGI set.
+	 */
 	private static void generateConfusionMatrix() {
+		//TODO this isn't right, look it over and fix it.  What is the unused hcGraph for?
 		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
 		ArrayList<DomainDomainInteraction> DDIs = EdgeFileReader.readDDIList();
 		ArrayList<GeneGeneInteraction> GGIs = EdgeFileReader.readGGIList();
@@ -66,15 +130,39 @@ public class Main {
 		tpGGIs.retainAll(GGIs); // Intersection gives all TP results
 		
 		System.out.println(GGIs.size() + "\t" + predictedGGIs.size() + "\t" + tpGGIs.size());
+		
+		
+		
+//		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
+//		ArrayList<DomainDomainInteraction> DDIs = EdgeFileReader.readDDIList();
+//		ArrayList<GeneGeneInteraction> GGIs = EdgeFileReader.readGGIList();
+//		
+//		HashSet<DomainDomainInteraction> assumed_DDI_set = NetworkFactory.convertGGItoDDI(predictedDomains, GGIs);
+//		HashSet<DomainDomainInteraction> DDI_intersection = new HashSet<DomainDomainInteraction>();
+//		DDI_intersection.addAll(DDIs);
+//		DDI_intersection.retainAll(assumed_DDI_set);
+//		
+//		UndirectedGraph<String, DefaultEdge> predictedGraph = NetworkFactory.generateNetwork(predictedDomains, DDIs); //DDI_intersection
+//		ArrayList<GeneGeneInteraction> predictedGGIs = new ArrayList<GeneGeneInteraction>();
+//		ArrayList<GeneGeneInteraction> tpGGIs = new ArrayList<GeneGeneInteraction>();
+//		predictedGGIs.addAll(NetworkFactory.convertEdgesToGGIs(predictedGraph));
+//		tpGGIs.addAll(predictedGGIs);
+//		tpGGIs.retainAll(GGIs); // Intersection gives all TP results
+//		
+//		System.out.println("HCSet\tPredictions\tTP");
+//		System.out.println(GGIs.size() + "\t" + predictedGGIs.size() + "\t" + tpGGIs.size());
 	}
 	
-
+	/**
+	 * Run at 10% test, 90% train cross validation using assumed DDIs generated from the high confidence GGI set.
+	 */
 	private static void crossValidate() {
+		int numRuns = 10;
 		ArrayList<PredictedDomain> predictedDomains = NodeFileReader.readNodeList();
 //		ArrayList<DomainDomainInteraction> DDIs = EdgeFileReader.readDDIList();
 		ArrayList<GeneGeneInteraction> GGIs = EdgeFileReader.readGGIList();
 		
-		for (int i=0; i<10; i++) {
+		for (int i=0; i<numRuns; i++) {
 			ArrayList<GeneGeneInteraction> tmp = new ArrayList<GeneGeneInteraction>();
 			ArrayList<GeneGeneInteraction> GGI_Test = new ArrayList<GeneGeneInteraction>();
 			ArrayList<GeneGeneInteraction> GGI_Train = new ArrayList<GeneGeneInteraction>();

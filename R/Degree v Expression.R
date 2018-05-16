0c9e51b2623ee933891407602f3a9d9b59f956aa
Jesse Walsh <jrwalsh@iastate.edu>
2018-05-16 15:17:55 -0500

    Degree vs Expression

--- R/Degree v Expression.R
+++ R/Degree v Expression.R
@@ -1,4 +1,23 @@
-data_out_hc3 <- read.delim("~/Desktop/data_out_hc3.tab")
-temp_data <- subset(data_out_hc3, data_out_hc3$Degree < 50)
-plot(log(data_out_hc3$Exp), log(data_out_hc3$Avg))
-lines(lowess(data_out_hc3$Degree, data_out_hc3$Avg), col="red")
+node_attr_ara_expression <- read.delim("~/git/PPI-Predictions/R/node_attr_ara_expression.tab")
+
+#temp_data <- subset(node_attr_ara_expression, node_attr_ara_expression$Degree < 50)
+
+# Degree vs. Expression
+plot(node_attr_ara_expression$Degree, node_attr_ara_expression$Exp)
+lines(lowess(node_attr_ara_expression$Degree, node_attr_ara_expression$Exp), col="red")
+
+  # Inverse relationship?
+plot(node_attr_ara_expression$Degree, log(node_attr_ara_expression$Exp))
+plot(log(node_attr_ara_expression$Degree), node_attr_ara_expression$Exp)
+  # Power law relationship?
+plot(log(node_attr_ara_expression$Degree), log(node_attr_ara_expression$Exp))
+
+# Expression vs. Avg
+plot(node_attr_ara_expression$Exp, node_attr_ara_expression$Avg)
+lines(lowess(node_attr_ara_expression$Exp, node_attr_ara_expression$Avg), col="red")
+
+# Inverse relationship?
+plot(node_attr_ara_expression$Exp, log(node_attr_ara_expression$Avg))
+plot(log(node_attr_ara_expression$Exp), node_attr_ara_expression$Avg)
+# Power law relationship?
+plot(log(node_attr_ara_expression$Exp), log(node_attr_ara_expression$Avg))
